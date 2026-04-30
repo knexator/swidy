829e82275b8b0351c5330e6399353ea1156ad339
knexator <daniel.hurtadoramirez314@gmail.com>
2026-04-30 19:10:51 +0200

    start thinking about dynamic linking

--- build.zig
+++ build.zig
@@ -28,4 +28,21 @@ pub fn build(b: *std.Build) void {
     const run_exe_tests = b.addRunArtifact(exe_tests);
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_exe_tests.step);
+
+    const stdlib = b.addLibrary(.{
+        .name = "stdlib",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/stdlib.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+        .linkage = .dynamic,
+    });
+    b.installArtifact(stdlib);
+
+    const stdlib_tests = b.addTest(.{
+        .root_module = stdlib.root_module,
+    });
+    const run_stdlib_tests = b.addRunArtifact(stdlib_tests);
+    test_step.dependOn(&run_stdlib_tests.step);
 }
---
+++ src/stdlib.zig
@@ -0,0 +1,17 @@
+const Swidy = @import("main.zig").Swidy;
+const std = @import("std");
+
+export fn example(swidy: *Swidy, value: Swidy.Value) Swidy.Value {
+    return swidy.buildPair(value, value);
+}
+
+test example {
+    var swidy: Swidy = .init(std.testing.allocator);
+    defer swidy.deinit();
+
+    const x = swidy.buildString("x");
+    const actual = example(&swidy, x);
+    const expected = swidy.buildPair(x, x);
+
+    try swidy.expectEqual(expected, actual);
+}
