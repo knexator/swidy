d64590da6995f140e51ad6d3667688a35a28bb85
knexator <daniel.hurtadoramirez314@gmail.com>
2026-05-05 17:52:44 +0200

    add_u32_u32

--- src/stdlib.zig
+++ src/stdlib.zig
@@ -15,3 +15,14 @@ test example {
 
     try swidy.expectEqual(expected, actual);
 }
+
+export fn add_u32_u32(swidy: *Swidy, value: Swidy.Value) callconv(.c) Swidy.Value {
+    const a = std.mem.readInt(u32, asFixedLength(4, swidy.get(swidy.get(value).pair.left).string), .native);
+    const b = std.mem.readInt(u32, asFixedLength(4, swidy.get(swidy.get(value).pair.right).string), .native);
+    return swidy.buildString(&std.mem.toBytes(a + b));
+}
+
+fn asFixedLength(comptime N: usize, slice: []const u8) *const [N]u8 {
+    if (slice.len != N) std.debug.panic("expected string of length {d}, found {s}", .{ N, slice });
+    return slice[0..N];
+}
