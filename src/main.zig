99691fa6b7df45bcf0e1736239251ae889eea60d
knexator <anon@anon.com>
2026-07-23 11:25:41 +0200

    refactor

--- src/main.zig
+++ src/main.zig
@@ -2,35 +2,64 @@ const std = @import("std");
 const Io = std.Io;
 const assert = std.debug.assert;
 const panic = std.debug.panic;
+const fatal = std.process.fatal;
 
 const core = @import("core.zig");
 const Swidy = core.Swidy;
 const Debugger = core.Debugger;
 
+/// This can be global since stdin is a singleton.
+var stdin_buffer: [4096]u8 align(std.heap.page_size_min) = undefined;
+/// This can be global since stdout is a singleton.
+var stdout_buffer: [4096]u8 align(std.heap.page_size_min) = undefined;
+
 pub fn main(init: std.process.Init) !u8 {
     const io = init.io;
 
-    var swidy: Swidy = .init(init.gpa);
+    var args = try init.minimal.args.iterateAllocator(init.arena.allocator());
+    assert(args.skip());
+
+    const mode = args.next() orelse "help";
+
+    if (std.mem.eql(u8, mode, "help")) {
+        try Io.File.stdout().writeStreamingAll(io,
+            \\Usage:
+            \\  swity run [files]
+            \\  swity debug
+            \\  swity lsp
+            \\
+        );
+    } else if (std.mem.eql(u8, mode, "lsp")) {
+        panic("TODO", .{});
+    } else if (std.mem.eql(u8, mode, "run")) {
+        panic("TODO", .{});
+    } else if (std.mem.eql(u8, mode, "debug")) {
+        try cmd_debug(init.gpa, io);
+    } else {
+        fatal("unknown command: {s}", .{mode});
+    }
+
+    return 0;
+}
+
+fn cmd_debug(gpa: std.mem.Allocator, io: std.Io) !void {
+    var stdout_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
+    const stdout = &stdout_writer.interface;
+    var stdin_reader: Io.File.Reader = .init(.stdin(), io, &stdin_buffer);
+    const stdin = &stdin_reader.interface;
+
+    var swidy: Swidy = .init(gpa);
     defer swidy.deinit();
 
     var debugger: Debugger = .init(&swidy);
 
-    var stdin_buffer: [1024]u8 = undefined;
-    var stdin_file_reader: Io.File.Reader = .init(.stdin(), io, &stdin_buffer);
-    const stdin_reader = &stdin_file_reader.interface;
-
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
-
     while (true) {
-        try stdout_writer.print("> ", .{});
-        try stdout_writer.flush();
-        const in = try Swidy.Parser.sexpr(&swidy, stdin_reader);
+        try stdout.print("> ", .{});
+        try stdout.flush();
+        const in = try Swidy.Parser.sexpr(&swidy, stdin);
         const out = debugger.do(in);
-        try stdout_writer.print("{f}\n", .{swidy.fmt(out)});
+        try stdout.print("{f}\n", .{swidy.fmt(out)});
     }
-    try stdout_writer.flush();
 
-    return 0;
+    try stdout_writer.flush();
 }
